a76bdccee84533ff20ab7a295036ecf96ad24cf9
Alexandr <alexandr.kolodnitskiy@gmail.com>
2022-02-21 15:06:27 +0700

    Add region exception

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    public class Gender
+    {
+        // Пол.
+        public string Name { get; }
+
+        // Название.
+
+        public Gender(string name)
+        {
+            // Создать новый пол
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
